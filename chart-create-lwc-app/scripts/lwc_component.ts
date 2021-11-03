95cd2219c7f721af233fb71441e4330c2ae8b581
Garrick Lee <garrick.lee@keysight.com>
2021-11-03 16:45:29 -0700

    Remove comments.

--- chart-create-lwc-app/scripts/lwc_component.ts
+++ chart-create-lwc-app/scripts/lwc_component.ts
@@ -55,7 +55,6 @@ async function migrate(argv: Arguments) {
     moduledirname = modDir.split('/').slice(-1)[0];
 
     const imports = await recurseNeededImports([rootcomp], { rootdir });
-    console.log('imports', imports)
     
     const tempdir = await cloneToTempDir(imports);
     // compressComponents(tempdir);
@@ -186,59 +185,3 @@ const filter_localModules = (item: ImportDeclaration | any) => {
 };
 
 const parseCode = (code: string): Program => parse(code, swc_config);
-
-/*
-
-{
-  "jsc": {
-    "parser": {
-      "syntax": "ecmascript",
-      "jsx": false,
-      "dynamicImport": false,
-      "privateMethod": false,
-      "functionBind": false,
-      "exportDefaultFrom": false,
-      "exportNamespaceFrom": false,
-      "decorators": false,
-      "decoratorsBeforeExport": false,
-      "topLevelAwait": false,
-      "importMeta": false
-    },
-    "transform": null,
-    "target": "es5",
-    "loose": false,
-    "externalHelpers": false,
-    // Requires v1.2.50 or upper and requires target to be es2016 or upper.
-    "keepClassNames": false
-  }
-}
-
-*/
-
-// console.log('ast', ast);
-
-// {
-//   type: "Module",
-//   span: { start: 0, end: 36, ctxt: 0 },
-//   body: [
-//     {
-//       type: "VariableDeclaration",
-//       span: [Object],
-//       kind: "const",
-//       declare: false,
-//       declarations: [Array]
-//     }
-//   ],
-//   interpreter: null
-// }
-
-// const { code: output } = print(ast, {
-//   minify: true,
-//   module: {
-//     type: "commonjs"
-//   }
-// });
-
-// console.log("output", output);
-
-// const x = "Hello, Deno SWC!"
