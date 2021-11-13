bfdb974a18b9f77975ee71f87737d87268b61efc
Garrick Lee <garrick.lee@keysight.com>
2021-11-12 17:08:29 -0800

    Baseline custom tree.

--- chart-create-lwc-app/src/client/modules/my/app/app.js
+++ chart-create-lwc-app/src/client/modules/my/app/app.js
@@ -958,13 +958,113 @@ export default class App extends LightningElement {
                 label: DCX_Asset_Service_Returned_Documents,
                 fieldName: 'download',
                 sortable: true,
-                type: "download",
+                type: 'download',
             },
         ],
         onEditRow: (e) => {
             console.log('onEditRow.e', e);
         },
     };
+
+    tree_items = [
+        {
+            label: 'Western Sales Director',
+            name: '1',
+            expanded: true,
+            items: [
+                {
+                    label: 'Western Sales Manager',
+                    name: '2',
+                    expanded: true,
+                    items: [
+                        {
+                            label: 'CA Sales Rep Rep Rep Rep Rep Rep ',
+                            name: '3',
+                            expanded: true,
+                            items: [],
+                        },
+                        {
+                            label: 'OR Sales Rep Rep Rep ',
+                            name: '4',
+                            expanded: true,
+                            items: [],
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            label: 'Eastern Sales Director',
+            name: '5',
+            expanded: false,
+            items: [
+                {
+                    label: 'Easter Sales Manager',
+                    name: '6',
+                    expanded: true,
+                    items: [
+                        {
+                            label: 'NY Sales Rep Rep Rep Rep Rep Rep Rep Rep ',
+                            name: '7',
+                            expanded: true,
+                            items: [],
+                        },
+                        {
+                            label: 'MA Sales Rep Rep Rep Rep Rep ',
+                            name: '8',
+                            expanded: true,
+                            items: [],
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            label: 'International Sales Director',
+            name: '9',
+            expanded: true,
+            items: [
+                {
+                    label: 'Asia Sales Manager',
+                    name: '10',
+                    expanded: true,
+                    items: [
+                        {
+                            label: 'Sales Rep1',
+                            name: '11',
+                            expanded: true,
+                            items: [],
+                        },
+                        {
+                            label: 'Sales Rep2',
+                            name: '12',
+                            expanded: true,
+                            items: [],
+                        },
+                    ],
+                },
+                {
+                    label: 'Europe Sales Manager',
+                    name: '13',
+                    expanded: false,
+                    items: [
+                        {
+                            label: 'Sales Rep1',
+                            name: '14',
+                            expanded: true,
+                            items: [],
+                        },
+                        {
+                            label: 'Sales Rep2',
+                            name: '15',
+                            expanded: true,
+                            items: [],
+                        },
+                    ],
+                },
+            ],
+        },
+    ];
 
     connectedCallback() {
         if (!this.datatable1.initialized) {
