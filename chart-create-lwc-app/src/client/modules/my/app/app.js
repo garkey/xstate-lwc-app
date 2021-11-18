4815e8455953901969d85cffd69304949b59c0ad
Garrick Lee <garrick.lee@keysight.com>
2021-11-18 10:25:02 -0800

    Baseline on dispatch event handler.

--- chart-create-lwc-app/src/client/modules/my/app/app.js
+++ chart-create-lwc-app/src/client/modules/my/app/app.js
@@ -966,105 +966,110 @@ export default class App extends LightningElement {
         },
     };
 
-    tree_items = [
-        {
-            label: 'Western Sales Director',
-            name: '1',
-            expanded: true,
-            items: [
-                {
-                    label: 'Western Sales Manager',
-                    name: '2',
-                    expanded: true,
-                    items: [
-                        {
-                            label: 'CA Sales Rep Rep Rep Rep Rep Rep ',
-                            name: '3',
-                            expanded: true,
-                            items: [],
-                        },
-                        {
-                            label: 'OR Sales Rep Rep Rep ',
-                            name: '4',
-                            expanded: true,
-                            items: [],
-                        },
-                    ],
-                },
-            ],
+    tree_items = {
+        data: [
+            {
+                label: 'Western Sales Director',
+                name: '1',
+                expanded: true,
+                items: [
+                    {
+                        label: 'Western Sales Manager',
+                        name: '2',
+                        expanded: true,
+                        items: [
+                            {
+                                label: 'CA Sales Rep Rep Rep Rep Rep Rep ',
+                                name: '3',
+                                expanded: true,
+                                items: [],
+                            },
+                            {
+                                label: 'OR Sales Rep Rep Rep ',
+                                name: '4',
+                                expanded: true,
+                                items: [],
+                            },
+                        ],
+                    },
+                ],
+            },
+            {
+                label: 'Eastern Sales Director',
+                name: '5',
+                expanded: false,
+                items: [
+                    {
+                        label: 'Easter Sales Manager',
+                        name: '6',
+                        expanded: true,
+                        items: [
+                            {
+                                label: 'NY Sales Rep Rep Rep Rep Rep Rep Rep Rep ',
+                                name: '7',
+                                expanded: true,
+                                items: [],
+                            },
+                            {
+                                label: 'MA Sales Rep Rep Rep Rep Rep ',
+                                name: '8',
+                                expanded: true,
+                                items: [],
+                            },
+                        ],
+                    },
+                ],
+            },
+            {
+                label: 'International Sales Director',
+                name: '9',
+                expanded: true,
+                items: [
+                    {
+                        label: 'Asia Sales Manager',
+                        name: '10',
+                        expanded: true,
+                        items: [
+                            {
+                                label: 'Sales Rep1',
+                                name: '11',
+                                expanded: true,
+                                items: [],
+                            },
+                            {
+                                label: 'Sales Rep2',
+                                name: '12',
+                                expanded: true,
+                                items: [],
+                            },
+                        ],
+                    },
+                    {
+                        label: 'Europe Sales Manager',
+                        name: '13',
+                        expanded: false,
+                        items: [
+                            {
+                                label: 'Sales Rep1',
+                                name: '14',
+                                expanded: true,
+                                items: [],
+                            },
+                            {
+                                label: 'Sales Rep2',
+                                name: '15',
+                                expanded: true,
+                                items: [],
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
+        handleAdd: (e) => {
+            console.log('e', e);
         },
-        {
-            label: 'Eastern Sales Director',
-            name: '5',
-            expanded: false,
-            items: [
-                {
-                    label: 'Easter Sales Manager',
-                    name: '6',
-                    expanded: true,
-                    items: [
-                        {
-                            label: 'NY Sales Rep Rep Rep Rep Rep Rep Rep Rep ',
-                            name: '7',
-                            expanded: true,
-                            items: [],
-                        },
-                        {
-                            label: 'MA Sales Rep Rep Rep Rep Rep ',
-                            name: '8',
-                            expanded: true,
-                            items: [],
-                        },
-                    ],
-                },
-            ],
-        },
-        {
-            label: 'International Sales Director',
-            name: '9',
-            expanded: true,
-            items: [
-                {
-                    label: 'Asia Sales Manager',
-                    name: '10',
-                    expanded: true,
-                    items: [
-                        {
-                            label: 'Sales Rep1',
-                            name: '11',
-                            expanded: true,
-                            items: [],
-                        },
-                        {
-                            label: 'Sales Rep2',
-                            name: '12',
-                            expanded: true,
-                            items: [],
-                        },
-                    ],
-                },
-                {
-                    label: 'Europe Sales Manager',
-                    name: '13',
-                    expanded: false,
-                    items: [
-                        {
-                            label: 'Sales Rep1',
-                            name: '14',
-                            expanded: true,
-                            items: [],
-                        },
-                        {
-                            label: 'Sales Rep2',
-                            name: '15',
-                            expanded: true,
-                            items: [],
-                        },
-                    ],
-                },
-            ],
-        },
-    ];
+    };
 
     connectedCallback() {
         if (!this.datatable1.initialized) {
